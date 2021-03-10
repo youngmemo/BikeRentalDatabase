9362ef5f91741bf66220f4adb0bcfd78d3505df0
Janis Gailis <janis.gailis@uia.no>
2021-03-10 13:56:43 +0100

    Added hellow

--- hellow.asm
+++ hellow.asm
@@ -1,18 +1,43 @@
-; ----------------------------------------------------------------------------------------
-; This is a Win64 console program that writes "Hello" on one line and then exits.  It
-; uses puts from the C library.  To assemble and run:
-;
-;     nasm -fwin64 hello.asm && gcc hello.obj && a
-; ----------------------------------------------------------------------------------------
+; Console Message, 64 bit. V1.03
+NULL              EQU 0                         ; Constants
+STD_OUTPUT_HANDLE EQU -11
 
-        global  main
-        extern  puts
-        section .text
-main:
-        sub     rsp, 28h                        ; Reserve the shadow space
-        mov     rcx, message                    ; First argument is address of message
-        call    puts                            ; puts(message)
-        add     rsp, 28h                        ; Remove shadow space
-        ret
-message:
-        db      'Hello', 0                      ; C strings need a zero byte at the end
+extern GetStdHandle                             ; Import external symbols
+extern WriteFile                                ; Windows API functions, not decorated
+extern ExitProcess
+
+global Start                                    ; Export symbols. The entry point
+
+section .data                                   ; Initialized data segment
+ Message        db "Console Message 64", 0Dh, 0Ah
+ MessageLength  EQU $-Message                   ; Address of this line ($) - address of Message
+
+section .bss                                    ; Uninitialized data segment
+alignb 8
+ StandardHandle resq 1
+ Written        resq 1
+
+section .text                                   ; Code segment
+Start:
+ sub   RSP, 8                                   ; Align the stack to a multiple of 16 bytes
+
+ sub   RSP, 32                                  ; 32 bytes of shadow space
+ mov   ECX, STD_OUTPUT_HANDLE
+ call  GetStdHandle
+ mov   qword [REL StandardHandle], RAX
+ add   RSP, 32                                  ; Remove the 32 bytes
+
+ sub   RSP, 32 + 8 + 8                          ; Shadow space + 5th parameter + align stack
+                                                ; to a multiple of 16 bytes
+ mov   RCX, qword [REL StandardHandle]          ; 1st parameter
+ lea   RDX, [REL Message]                       ; 2nd parameter
+ mov   R8, MessageLength                        ; 3rd parameter
+ lea   R9, [REL Written]                        ; 4th parameter
+ mov   qword [RSP + 4 * 8], NULL                ; 5th parameter
+ call  WriteFile                                ; Output can be redirect to a file using >
+ add   RSP, 48                                  ; Remove the 48 bytes
+
+ xor   ECX, ECX
+ call  ExitProcess
+
+
